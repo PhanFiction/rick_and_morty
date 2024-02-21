297df41c6c8b38f44bc99900a682783d5c6a87f1
Steve <stevewu24@gmail.com>
2024-02-20 23:11:21 -0800

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        yellow: "#FAFD7CFF",
+        darkBrown: "#82491EFF",
+        navyBlue: "#24325FFF",
+        lightBlue: "#B7E4F9FF",
+        lightRed: "#FB6467FF",
+        darkGreen: "#526E2DFF",
+        pink: "#E762D7FF",
+        orange: "#E89242FF",
+        darkYellow: "#FAE48BFF",
+        cyan: "#A6EEE6FF",
+        brown: "#917C5DFF",
+        skyBlue: "#69C8ECFF",
+        lightGreen: "#86FF4B",
+        lighterGreen: '#8EFF8B',
+        lightGray: "#AEAEAE",
+      },
       backgroundImage: {
         'rickandmorty-bg': "url('./src/assets/bg-rickandmorty.png')",
         'rickandmorty-circle': "url('./src/assets/circle-rickandmorty.png)",

d71e4527d6db8b3a7cb7f1680ba25b147daa291c
Steve <stevewu24@gmail.com>
2024-02-29 12:29:23 -0800

    added animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ export default {
         lightGreen: "#86FF4B",
         lighterGreen: '#8EFF8B',
         lightGray: "#AEAEAE",
+        darkNavy: "#23232e",
       },
       backgroundImage: {
         'rickandmorty-bg': "url('./src/assets/bg-rickandmorty.png')",
@@ -32,6 +33,40 @@ export default {
       fontSize: {
         clamp: "clamp(1rem, 2vw, 3rem)",
       },
+      animation: {
+        'text-slide-in': 'text-slide-in 0.2s cubic-bezier(.17,.67,.83,.67) 0.1s both',
+        'text-slide-out': 'text-slide-out 0.15s cubic-bezier(.17,.67,.83,.67) 0.1s both'
+      },
+      keyframes: {
+        'text-slide-in': {
+          '0%': {
+            transform: 'translateX(-40%)',
+            opacity: '0',
+          },
+          '50%': {
+            transform: 'translateX(-20%)',
+            opacity: '0.25',
+          },
+          '100%': {
+            transform: 'translateX(0%)',
+            opacity: '1',
+          },
+        },
+        'text-slide-out': {
+          '0%': {
+            transform: 'translateX(0%)',
+            opacity: '1',
+          },
+          '50%': {
+            transform: 'translateX(-50%)',
+            opacity: '0.25',
+          },
+          '100%': {
+            transform: 'translateX(-100%)',
+            opacity: '0',
+          },
+        }         
+    },
     },
   },
   plugins: [],

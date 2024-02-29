02779c329892c039fa39c6f6b37ac01718a6c91e
Steve <stevewu24@gmail.com>
2024-02-29 12:29:05 -0800

    added home page and planet page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,35 +1,18 @@
-import { useEffect, useContext } from 'react';
-import { PageContext } from '../../helpers/PageContext';
-import { OverLay } from '../../components/Model/Overlay/Overlay';
+import OverLay from '../../components/OverLay/OverLay';
 import rickandmortyCircle from '../../assets/circle-rickandmorty.png';
 import rickandmortyTitle from '../../assets/title-rickandmorty.png';
+import { usePageLocation } from '../../hooks/usePageLocation';
 
 export default function HomePage() {
-  const { setPage } = useContext(PageContext);
-  
-  useEffect(() => {
-    setPage('garage-slide-door');
-  }, []);
+  usePageLocation('garage-slide-door');
 
   return (
     <>
       <OverLay>
-        <div className='relative -top-24 sm:bottom-0 left-0 right-0'>
-          <div className='flex items-center justify-center'>
-            <div className='absolute -bottom-24'>
-              <input
-                type="search"
-                placeholder='Search'
-                className='w-72 md:w-96 p-4 rounded-lg bg-white'
-              />
-              <div className='absolute w-full h-[160px] sm:h-[120px] top-11 rounded-lg bg-red-50'>
-                
-              </div>
-            </div>
-          </div>
-          <div className='flex justify-center'>
-            <img src={rickandmortyCircle } alt="circle" className='w-[100%] md:w-[60%] lg:w-[75%]'/>
-            <img src={rickandmortyTitle} alt="title" className='w-[70%] sm:w-[65%] md:w-[55%] lg:w-[50%] absolute bottom-0'/>
+        <div className='w-full h-screen flex flex-col'>
+          <div className='relative flex justify-center flex-col items-center w-full -top-12'>
+            <img src={rickandmortyCircle } alt="circle" className='w-[100%] sm:w-[60%] lg:w-[50%]'/>
+            <img src={rickandmortyTitle} alt="title" className='w-[70%] sm:w-[45%] lg:w-[35%] bottom-0 absolute'/>
           </div>
         </div>
       </OverLay>
--- src/pages/PlanetPage/PlanetPage.jsx
+++ src/pages/PlanetPage/PlanetPage.jsx
@@ -1,14 +1,53 @@
-import { useEffect, useContext } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { PageContext } from '../../helpers/PageContext';
+import OverLay from '../../components/OverLay/OverLay';
+import { usePageLocation } from '../../hooks/usePageLocation';
+import Searchbar from '../../components/Searchbar/Searchbar';
+import { useFetchData, useSearchLocation } from '../../hooks/useFetchData';
+import rickandmortyCircle from '../../assets/circle-rickandmorty.png';
+import rickandmortyTitle from '../../assets/title-rickandmorty.png';
 
 export default function PlanetPage() {
   const { setPage } = useContext(PageContext);
-  
-  useEffect(() => {
-    setPage('shelf');
-  }, []);
+  usePageLocation('washingmachine');
+  const response = useSearchLocation('earth');
+  const { results, info } = response;
+  const [userInput, setUserInput] = useState('');
 
   return (
-    <div>PlanetPage</div>
+    <OverLay>
+      <div className='w-full h-screen flex flex-col'>
+
+        <div className='relative flex justify-center flex-col items-center w-full -top-12'>
+          <img src={rickandmortyCircle } alt="circle" className='w-[100%] sm:w-[60%] lg:w-[50%]'/>
+          <img src={rickandmortyTitle} alt="title" className='w-[70%] sm:w-[45%] lg:w-[35%] bottom-0 absolute'/>
+        </div>
+
+        <form className='w-full'>
+          <Searchbar locations={results} />
+        </form>
+
+        <article className='flex flex-col text-white'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-16 ml-24 mr-5'>
+{/*             {
+              results?.map(character => (
+                <div className='rounded-md border-2 border-lightGreen custom-box-shadow residentCard cursor-pointer bg-black' key={character.id}>
+                  <img src={`${character.image}`} alt="" className='w-full opacity-90'/>
+                  <h2 className='text-lighterGreen font-semibold text-2xl text-center my-2'>{ character.name }</h2>
+                  <div className='my-4'>
+                    <ul className='px-8 [&>li]:my-1'>
+                      <li><span className="text-lightGray">Gender: </span>{ character.gender }</li>
+                      <li><span className="text-lightGray">Species: </span>{ character.species }</li>
+                      <li><span className="text-lightGray">Location: </span>{ character.location.name }</li>
+                      <li><span className="text-lightGray">Status: </span>{ character.status }</li>
+                    </ul>
+                  </div>
+                </div>
+              ))
+            } */}
+          </div>
+        </article>
+      </div>
+    </OverLay>
   )
 }

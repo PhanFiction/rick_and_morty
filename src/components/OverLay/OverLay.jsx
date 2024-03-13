a60ff232a571e9717018d847e0db2be35008fe82
Steve <stevewu24@gmail.com>
2024-03-12 23:26:12 -0700

    removed absolute to fixed

--- src/components/OverLay/OverLay.jsx
+++ src/components/OverLay/OverLay.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 export default function OverLay({ children }) {
   return (
     <>
-      <div className='absolute top-0 left-0 right-0 min-h-screen overflow-auto z-10'>{ children }</div>
-      <div className='absolute top-0 left-0 right-0 min-h-screen bg-rickandmorty-bg opacity-80 bg-cover'></div>
+      <div className='fixed top-0 left-0 right-0 min-h-screen overflow-auto z-10'>{ children }</div>
+      <div className='fixed top-0 left-0 right-0 min-h-screen bg-rickandmorty-bg opacity-80 bg-cover'></div>
     </>
   )
 }

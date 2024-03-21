ff18285d96559ac10a351f1b7c917916bd08be35
Steve <stevewu24@gmail.com>
2024-03-20 21:52:58 -0700

    updated CardContent component

--- src/components/Card/CardContent.jsx
+++ src/components/Card/CardContent.jsx
@@ -1,9 +1,9 @@
 export default function CardContent({ character }) {
   return (
-    <>
-      <img src={`${character?.image}`} alt="" className='w-full opacity-90'/>
-      <h2 className='text-lighterGreen font-semibold text-2xl text-center my-2'>{ character?.name }</h2>
+    <div className="flex flex-wrap">
+      <img src={`${character?.image}`} alt="img" className='opacity-90 w-full'/>
       <div className='my-4'>
+        <h2 className='text-lighterGreen font-semibold text-2xl text-center my-2'>{ character?.name }</h2>
         <ul className='px-8 [&>li]:my-1 [&>li>span]:text-lightGray text-left'>
           <li><span>Gender: </span>{ character?.gender }</li>
           <li><span>Species: </span>{ character?.species }</li>
@@ -11,6 +11,7 @@ export default function CardContent({ character }) {
           <li><span>Status: </span>{ character?.status }</li>
         </ul>
       </div>
-    </>
+
+    </div>
   )
 }
